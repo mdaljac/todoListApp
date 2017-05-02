3d3fa500b2ef7a5f0c19bb9bc248e5db5a23583c
mdaljac <mdaljac@gmail.com>
2017-05-03 01:38:06 +0200

    Delete functionality
    Delete button working

--- assets/js/todoList.js
+++ assets/js/todoList.js
@@ -1,6 +1,22 @@
 $(document).ready(function($) {
 	
-	
+	$("li, .delete-btn").on("click", function(){
+
+		//delete
+		if ($(this).hasClass('delete-btn')){
+
+			$listItem = $(this).parent("li");
+			$listItem.css("text-decoration", "line-through");
+			$listItem.slideUp(300, function(){
+					$(this).remove();
+			});
+		}
+		//toggle strike through
+		else{
+			$(this).toggleClass('strike');
+		}
+
+	});
 
 
 });
